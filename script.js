4d8187e5548dc3652e037ac4df79f8314c7c0aca
AdhamMeyliyev <adhammeyliyev8@gmail.com>
2024-02-08 09:49:31 +0500

    team

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+    const splide = new Splide( '.splide', {
+        perPage: 1,
+        autoplay:true,
+
+    } );
+    splide.mount();
+  } );
